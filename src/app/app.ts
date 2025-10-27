4ef3ef15384beb549cd379948c670d3b3b70c47b
Vivekanand Pujar <vivekanandpv@gmail.coim>
2025-10-27 16:21:27 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('learning-session1');
+  message: string = 'Hello, there!';
+  title: string = 'This is a title';
+  color: string = '#ff001e';
 }
