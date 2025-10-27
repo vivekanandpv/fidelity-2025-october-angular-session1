37219a1aada7c2b9ec17ba1bf7cacef165a2133e
Vivekanand Pujar <vivekanandpv@gmail.coim>
2025-10-27 14:44:42 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CurrencyPipe, DatePipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PercentPipe, CurrencyPipe, DatePipe, DecimalPipe],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('learning-session1');
+  message: string = 'Good afternoon!';
+  roi: number = 0.12;
+  price: number = 900.851234;
+  today: Date = new Date();
+  pi: number = 3.14159265359;
 }
