72bd92de7f79be01f22717521f806e280d657446
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-10-28 11:03:19 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,9 +12,45 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   count = signal(0);
+  doubleCount = computed(() => {
+    console.log('double counter evaluated');
+    return this.count() * 2;
+  }); //  lazily evaluated, read-only, memoized
+
+  person = signal<Person>({
+    firstName: 'John',
+    lastName: 'Doe',
+  });
+
+  colors = signal(['blue', 'green', 'red']);
 
   increment() {
-    this.count.set(this.count() + 1); //  also works
-    // this.count.update((v) => v + 1); //  recommended; captures earlier value
+    // this.count.set(this.count() + 1);
+    this.count.update((v) => v + 1);
+  }
+
+  changeDetails() {
+    this.person.update(() => ({
+      firstName: 'Rajan',
+      lastName: 'Kumar',
+    }));
+  }
+
+  addColor() {
+    this.colors.update((a) => [...a, 'black']);
+  }
+
+  constructor() {
+    //  effect can only be declared in an injection context,
+    // so that it is released when the component goes out of scope
+    //  This mostly means, you write effects in the constructor.
+
+    //  use effects sparingly
+    //  effects are used to cause "side-effects" such as logging, storing to localStorage, etc.
+    //  do not set/update signals inside the effects.
+    //  effects run asynchronously
+    effect(() => {
+      console.log('Current counter is:', this.count());
+    });
   }
 }
