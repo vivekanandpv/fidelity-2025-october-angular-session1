d2130df4c1b19b1879ddfac3c6076efb75adfbbb
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-10-28 14:28:29 +0530

    done

---
+++ src/app/sample/sample.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  imports: [],
+  templateUrl: './sample.html',
+  styleUrl: './sample.scss',
+})
+export class Sample {
+
+}
