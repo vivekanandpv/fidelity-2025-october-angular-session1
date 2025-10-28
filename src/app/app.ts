80af9ff1a2e0cc3546fbf41f8a129f90400cce20
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-10-28 16:14:29 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal, ViewChild } from '@angular/core';
 import { Sample } from './sample/sample';
 
 @Component({
@@ -7,10 +7,23 @@ import { Sample } from './sample/sample';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements AfterViewInit {
+  //  Component query is an advanced technique, where you are querying the components
+  //  that you used in the template, so that you can configure them to work with your component
+  //  This technique is used when you want to associate a paginator with a table, where both table and paginator
+  //  are separate components and yet, they have to work together.
+  //  Avoid the temptation to manipulate the components in this way! It can result in a spaghetti code.
+  //  Always prefer the most declarative and idiomatic way of Angular development.
+
+  @ViewChild(Sample) sample!: Sample;
+
   parentMessage = signal('Good afternoon!');
 
   messageHandler(m: string) {
     console.log('Parent component got:', m);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.sample.message());
   }
 }
